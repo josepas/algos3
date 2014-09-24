f3401653e51ef271e66caa66960f614ae8bbee29
Jose Pascarella <josepas979@gmail.com>
2014-09-24 15:43:21 -0430

    Constructor de la clase Nodo

--- proyecto1/Nodo.java
+++ proyecto1/Nodo.java
@@ -1,25 +1,50 @@
+import java.util.HashSet;
+import java.util.Iterator;
+
 public class Nodo {
 
     // Campos de la clase Nodo
     // 
     public String[] nombre;
-    public CONJUNTO adyacentes;
+    public HashSet adyacentes;
         
     // Constructor de Nodo
     public Nodo(String[] nuevoNombre) {
-        this(nuevoNombre, null);
+        this( nuevoNombre, new HashSet() );
     }
-    public Nodo(String[] nuevoNombre, CONJUNTO nuevoAdya) {
-        this(nuevoNombre, nuevoAdya);
-    }
-    public Nodo(String[] nuevoNombre, CONJUNTO nuevoAdya) {
+    public Nodo(String[] nuevoNombre, HashSet nuevoAdya) {
         this.nombre = nuevoNombre;
         this.adyacentes = nuevoAdya;
     }
            
+    /*
     // Metodos de Nodo
-    public void setCadence(int newValue) {
-        cadence = newValue;
+    public void obtenerNombre() {
+        System.out.println(nombre);
     }
-        
+
+    public void obtenerAdyacentes() {
+        System.out.println(nombre);
+    }
+    public void agregarAdyacente() {
+        System.out.println(nombre);
+    }
+     
+    public void agregarVariosAdyacente() {
+        System.out.println(nombre);
+    }    
+    public void imprimir() {
+        System.out.println("[" + this.nombre + "] -> [");
+        itr = this.adyacentes.iterator();
+        System.out.print( itr.next() );
+        while(itr.hasNext()) {
+            String i = itr.next();
+            System.out.print(",  " + i);
+        }
+        System.out.println("]");
+
+    }
+    */
 }
+
+
