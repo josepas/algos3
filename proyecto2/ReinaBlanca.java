1041ec7fd5d9f30e4568794ff75fde8e335ea9c6
Jose Pascarella <josepas979@gmail.com>
2014-10-23 15:16:49 -0430

    terminamos reinablanca

--- proyecto2/ReinaBlanca.java
+++ proyecto2/ReinaBlanca.java
@@ -1,33 +1,32 @@
 import java.util.*;
 
-
 class ReinaBlanca {
 
-	public void minimizarMonedas(char[][] reino, boolean[][] visit, int n) {
+	public static int minimizarMonedas(int[][] reino, boolean[][] visit, int n) {
 		
 		int i,j,k;		// Iteradores
 		int mi,mj, min; // Minimo de la matriz
 		ConjDisjuntos rb = new ConjDisjuntos(n);
+		boolean arbolCM;
 		int resultado = 0;
 
-		mi=1;
-		mj=1;
-		while (mi != 0 && mj != 0) {
-
+		min = 0;
+		while (min != 1000) {
 			mi = 0;
 			mj = 0;
-			min = 1000000;
+			min = 1000;
 			// Consigo el camino mas barato
 			for (i=0; i<n; i++) {
 				for (j=i+1; j<n; j++) {
-					if ( (reino[i][j] < min) && !visit[i][j]) {
+
+					if ( (reino[i][j] < min) && !visit[i][j] && reino[i][j] != -1) {
 						mi = i;
 						mj = j;
 						min = reino[i][j];
-						visit[i][j] = true;
 					}
 				}
 			}		
+			visit[mi][mj] = true;
 
 			if ( rb.representante(mi) != rb.representante(mj) ) {
 				rb.union(mi,mj);
@@ -35,14 +34,18 @@ class ReinaBlanca {
 			}
 
 			// Se consiguio ya el arbol cobertor?
+			arbolCM = true;
 			for (i=0; i<n; i++) {
 				if ( rb.representante(i) != rb.representante(0) ) {
-					continue;
+					arbolCM = false;
 				}
+			}
+			if (arbolCM) {
+				return resultado;
 			}
 
 		}
-
+		return -1;
 	}
 
 
@@ -50,11 +53,12 @@ class ReinaBlanca {
 		
 		int n;				// Numero de localidades
 		int i,j,k;			// Iteradores
+		int resultado;		// 
+
 		boolean[][] visit;	// Matriz de Visitados
 		int[][] reino;
-		char[] aux;
-					
 		
+		char[] aux;
 		String linea;
 		Scanner sc = new Scanner(System.in);
 
@@ -64,10 +68,7 @@ class ReinaBlanca {
 		visit = new boolean[n][n];
 		reino = new int[n][n];
 		
-
 		// Lectura de la matriz
-		
-		System.out.println(n);
 		for (i=0; i<n; i++) {
  			linea = sc.next();
  			aux = linea.toCharArray();
@@ -76,14 +77,19 @@ class ReinaBlanca {
 					reino[i][j] = -1; 
 				} else {
 					reino[i][j] = Character.digit(aux[j], 10);
-				}
 
-				System.out.print(reino[i][j]);
+				}
 				visit[i][j] = false;
 			}
-			System.out.println();
 		}
-		// minimizarMonedas(reino,);
+
+		resultado = minimizarMonedas(reino,visit,n);
+
+		if ( resultado == -1 ) {
+			System.out.println("FAIL");
+		} else {
+			System.out.println(resultado);
+		}
 
 	}
 
