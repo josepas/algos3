d72a2b4a6b54ff5288701c281d4a5572bf64af8d
Jose Pascarella <josepas979@gmail.com>
2014-11-25 11:19:53 -0430

    entrega solucion monascal sin comentarios

---
+++ proyecto3/entregaMonascal/Nodo.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Nodo {
+
+	private int pago;
+	private Nodo padre;
+	private String nombre;
+
+	public Nodo(String nombre, int pago, Nodo padre) {
+		this.nombre = nombre;
+		this.pago = pago;
+		this.padre = padre;
+
+	}
+	public String obtenerNombre() {
+		return this.nombre;
+	}
+
+	public Nodo obtenerPadre() {
+		return this.padre;
+	}
+
+	public int obtenerPago() {
+		return this.pago;
+	}
+
+	public void cambiarPago(int nuevoPago) {
+		this.pago = nuevoPago;
+	}
+
+	public void cambiarPadre(Nodo nodo) {
+		this.padre = nodo;
+	}
+
+}
